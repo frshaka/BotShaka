57ca3c58eb207cb52afebffe6e179f6df2f1ac87
felipqsquall <felipe.squall@gmail.com>
2024-09-14 14:52:58 -0300

    fix: Alterada versão do chatGPT para que sempre utilize a versão latest

--- botshaka.js
+++ botshaka.js
@@ -121,63 +121,6 @@ io.on('connection', function(socket) {
   });
 });
 
-/*// Função para criar subdiretório no Google Drive
-async function createSubdirectory(auth, parentFolderId, subdirectoryName) {
-  const driveService = google.drive({ version: 'v3', auth });
-
-  // Verificar se o subdiretório já existe
-  const existingFolderResponse = await driveService.files.list({
-    q: `'${parentFolderId}' in parents and name='${subdirectoryName}' and mimeType='application/vnd.google-apps.folder'`,
-    fields: 'files(id, name)',
-    spaces: 'drive'
-  });
-
-  if (existingFolderResponse.data.files.length > 0) {
-    // Subdiretório já existe
-    console.log('Subdiretório já existe:', existingFolderResponse.data.files[0].id);
-    return existingFolderResponse.data.files[0].id;
-  } else {
-    // Criar novo subdiretório
-    const fileMetadata = {
-      'name': subdirectoryName,
-      'mimeType': 'application/vnd.google-apps.folder',
-      'parents': [parentFolderId]
-    };
-
-    const folder = await driveService.files.create({
-      resource: fileMetadata,
-      fields: 'id'
-    });
-
-    console.log('Subdiretório criado com sucesso:', folder.data.id);
-    return folder.data.id;
-  }
-}
-
-// Função para fazer upload no Google Drive
-async function uploadToGoogleDrive(auth, fileName, filePath, parentFolderId) {
-  const driveService = google.drive({ version: 'v3', auth });
-
-  const fileMetadata = {
-    'name': fileName,
-    'parents': [parentFolderId]
-  };
-
-  const media = {
-    mimeType: mime.lookup(filePath),
-    body: fs.createReadStream(filePath)
-  };
-
-  const response = await driveService.files.create({
-    resource: fileMetadata,
-    media: media,
-    fields: 'id'
-  });
-
-  return response.data;
-}*/
-
-
 //EXECUÇÃO DAS AÇÕES EXTERNAS
 
 const groupId = '120363198603699526@g.us'; // ID do grupo que você quer monitorar
